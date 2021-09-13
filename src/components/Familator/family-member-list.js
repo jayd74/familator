e68cb95a70daf6b771440d13b97629fad5be461b
Veronica Wong <veronicawong@veronica-mbp.lan>
2021-09-13 00:29:53 -0400

    Added more relations

--- src/components/Familator/family-member-list.js
+++ src/components/Familator/family-member-list.js
@@ -6,482 +6,692 @@ export const familyMemberList = [
   {
     relationship: "Son",
     title: "兒子",
+    engTitle: "Son",
   },
   {
     relationship: "Daughter",
     title: "女兒",
+    engTitle: "Daughter",
   },
   {
     relationship: "Father",
     title: "父親",
+    engTitle: "Father",
   },
   {
     relationship: "Mother",
     title: "母親",
+    engTitle: "Mother",
   },
   {
     relationship: "Older Brother",
     title: "哥哥",
+    engTitle: "Older Brother",
   },
   {
     relationship: "Younger Brother",
     title: "弟弟",
+    engTitle: "Younger Brother",
   },
   {
     relationship: "Older Sister",
     title: "姐姐",
+    engTitle: "Older Sister",
   },
   {
     relationship: "Younger Sister",
     title: "妹妹",
+    engTitle: "Younger Sister",
   },
   {
     relationship: "Wife",
     title: "老婆 / 妻子",
+    engTitle: "Wife",
   },
   {
     relationship: "Husband",
     title: "老公 / 丈夫",
+    engTitle: "Husband",
   },
   // Dad's side
   {
     relationship: "Father's Father",
     title: "爺爺",
+    engTitle: "Paternal Grandfather",
   },
   {
     relationship: "Father's Mother",
     title: "嫲嫲 / 奶奶",
+    engTitle: "Paternal Grandmother",
   },
   {
     relationship: "Father's Older Brother",
     title: "伯伯",
+    engTitle: "Uncle",
   },
   {
     relationship: "Father's Older Brother's Wife",
     title: "伯母",
+    engTitle: "Aunt",
   },
   {
     relationship: "Father's Younger Brother",
     title: "叔叔",
+    engTitle: "Uncle",
   },
   {
     relationship: "Father's Younger Brother's Wife",
     title: "嬸嬸",
+    engTitle: "Aunt",
   },
   {
     relationship: "Father's Older Sister",
     title: "姑媽",
+    engTitle: "Aunt",
   },
   {
     relationship: "Father's Older Sister's Husband",
     title: "姑夫",
+    engTitle: "Uncle",
   },
   {
     relationship: "Father's Younger Sister's Husband",
     title: "姑夫",
+    engTitle: "Uncle",
   },
   {
     relationship: "Father's Younger Sister",
     title: "姑姐 / 姑姑",
+    engTitle: "Aunt",
   },
   // Mother's side
   {
     relationship: "Mother's Father",
     title: "公公 / 外公",
+    engTitle: "Maternal Grandfather",
   },
   {
     relationship: "Mother's Mother",
     title: "婆婆 / 姥姥 / 外祖母",
+    engTitle: "Maternal Grandmother",
   },
   {
     relationship: "Mother's Older Brother",
     title: "舅夫",
+    engTitle: "Uncle",
   },
   {
     relationship: "Mother's Younger Brother",
     title: "舅夫",
+    engTitle: "Uncle",
   },
   {
     relationship: "Mother's Older Brother's Wife",
     title: "舅母",
+    engTitle: "Aunt",
   },
   {
     relationship: "Mother's Younger Brother's Wife",
     title: "嬸嬸",
-  },
-  {
-    relationship: "Mother's Older Sister's Husband",
-    title: "姑夫",
-  },
-  {
-    relationship: "Mother's Younger Sister's Husband",
-    title: "姑夫",
+    engTitle: "Aunt",
   },
   {
     relationship: "Mother's Older Sister",
     title: "姨妈",
+    engTitle: "Aunt",
   },
   {
     relationship: "Mother's Younger Sister",
     title: "阿姨",
+    engTitle: "Aunt",
+  },
+  {
+    relationship: "Mother's Older Sister's Husband",
+    title: "姑夫",
+    engTitle: "Uncle",
+  },
+  {
+    relationship: "Mother's Younger Sister's Husband",
+    title: "姑夫",
+    engTitle: "Uncle",
   },
   // Cousins
   {
     relationship: "Father's Older Brother's Son",
     title: "堂兄 / 堂弟",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Father's Younger Brother's Son",
     title: "堂兄 / 堂弟",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Father's Older Sister's Son",
     title: "堂兄 / 堂弟",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Father's Younger Sister's Son",
     title: "堂兄 / 堂弟",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Father's Older Brother's Daughter",
     title: "堂姐 / 堂妹",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Father's Younger Brother's Daughter",
     title: "堂姐 / 堂妹",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Father's Older Sister's Daughter",
     title: "堂姐 / 堂妹",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Father's Younger Sister's Daughter",
     title: "堂姐 / 堂妹",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Mother's Older Brother's Son",
     title: "表哥 / 表弟",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Mother's Younger Brother's Son",
     title: "表哥 / 表弟",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Mother's Older Sister's Son",
     title: "表哥 / 表弟",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Mother's Younger Sister's Son",
     title: "表哥 / 表弟",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Mother's Older Brother's Daughter",
     title: "表姐 / 表妹",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Mother's Younger Brother's Daughter",
     title: "表姐 / 表妹",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Mother's Older Sister's Daughter",
     title: "表姐 / 表妹",
+    engTitle: "First Cousin",
   },
   {
     relationship: "Mother's Younger Sister's Daughter",
     title: "表姐 / 表妹",
+    engTitle: "First Cousin",
   },
   // Spouse's side - Wife
   {
     relationship: "Wife's Father",
     title: "外父 / 岳父",
+    engTitle: "Father-in-law",
   },
   {
     relationship: "Wife's Mother",
     title: "外母 / 岳母",
+    engTitle: "Mother-in-law",
   },
   {
     relationship: "Wife's Older Sister",
     title: "大姨子",
+    engTitle: "Sister-in-law",
   },
   {
     relationship: "Wife's Older Sister's Husband",
     title: "大姑子",
+    engTitle: "Sister-in-law's Husband", // not sure
   },
   {
     relationship: "Wife's Younger Sister",
     title: "小姨子",
+    engTitle: "Sister-in-law",
   },
   {
     relationship: "Wife's Younger Sister's Husband",
     title: "小姑子",
+    engTitle: "Sister-in-law's Husband", // not sure
   },
   {
     relationship: "Wife's Older Brother",
     title: "大舅子",
+    engTitle: "Brother-in-law",
   },
   {
     relationship: "Wife's Older Brother's Wife",
     title: "大舅嫂 / 大妗子",
+    engTitle: "Brother-in-law's Wife",
   },
   {
     relationship: "Wife's Younger Brother",
     title: "小叔子",
+    engTitle: "Brother-in-law",
   },
   {
     relationship: "Wife's Younger Brother's Wife",
     title: "小婶子",
+    engTitle: "Brother-in-law's Wife",
   },
   // Spouse's side - Husband
   {
     relationship: "Husband's Father",
     title: "老爺 / 岳父",
+    engTitle: "Father-in-law",
   },
   {
     relationship: "Husband's Mother",
     title: "奶奶 / 岳母",
+    engTitle: "Mother-in-law",
   },
   {
     relationship: "Husband's Older Sister",
     title: "大姑",
+    engTitle: "Sister-in-law",
   },
   {
     relationship: "Husband's Older Sister's Husband",
     title: "大姑子",
+    engTitle: "Sister-in-law's Husband",
   },
   {
     relationship: "Husband's Younger Sister",
     title: "小姑",
+    engTitle: "Sister-in-law",
   },
   {
     relationship: "Husband's Younger Sister's Husband",
     title: "小姑子",
+    engTitle: "Sister-in-law's Husband",
   },
   {
     relationship: "Husband's Older Brother",
     title: "大伯子",
+    engTitle: "Brother-in-law",
   },
   {
     relationship: "Husband's Older Brother's Wife",
     title: "大伯嫂",
+    engTitle: "Brother-in-law's Wife",
   },
   {
     relationship: "Husband's Younger Brother",
     title: "小叔子",
+    engTitle: "Brother-in-law",
   },
   {
     relationship: "Husband's Younger Brother's Wife",
     title: "小婶子",
+    engTitle: "Brother-in-law's Wife",
   },
   // Siblings side
   {
     relationship: "Older Brother's Wife",
     title: "嫂子",
+    engTitle: "Sister-in-law",
   },
   {
     relationship: "Younger Brother's Wife",
     title: "弟妹",
+    engTitle: "Sister-in-law",
   },
   {
     relationship: "Older Sisters's Wife",
     title: "姐夫",
+    engTitle: "Sister-in-law",
   },
   {
     relationship: "Younger Sisters's Wife",
     title: "妹夫",
+    engTitle: "Sister-in-law",
   },
   {
     relationship: "Older Brother's Son",
     title: "侄子",
+    engTitle: "Nephew",
   },
   {
     relationship: "Younger Brother's Son",
     title: "侄子",
+    engTitle: "Nephew",
   },
   {
     relationship: "Older Brother's Daugher",
     title: "侄女",
+    engTitle: "Niece",
   },
   {
     relationship: "Younger Brother's Daughter",
     title: "侄女",
+    engTitle: "Niece",
   },
   {
     relationship: "Older Sister's Son",
     title: "外甥",
+    engTitle: "Nephew",
   },
   {
     relationship: "Younger Sister's Son",
     title: "外甥",
+    engTitle: "Nephew",
   },
   {
     relationship: "Older Sister's Daughter",
     title: "外甥女",
+    engTitle: "Niece",
   },
   {
     relationship: "Younger Sister's Daughter",
     title: "外甥女",
+    engTitle: "Niece",
   },
   // Grandparents relatives
   {
     relationship: "Father's Father's Older Sister",
     title: "姑奶奶",
+    engTitle: "Grandaunt",
   },
   {
     relationship: "Father's Father's Older Sister's Husband",
     title: "姑爺",
+    engTitle: "Granduncle-in-law",
   },
   {
     relationship: "Father's Father's Older Brother",
     title: "大爺 / 二爺 ...",
+    engTitle: "Granduncle",
   },
   {
     relationship: "Father's Father's Older Brother's Wife",
     title: "大奶奶 / 二奶奶...",
+    engTitle: "Grandaunt-in-law",
   },
   {
     relationship: "Father's Father's Younger Sister",
     title: "姑奶奶",
+    engTitle: "Grandaunt",
   },
   {
     relationship: "Father's Father's Younger Sister's Husband",
     title: "姑爺",
+    engTitle: "Granduncle-in-law",
   },
   {
     relationship: "Father's Father's Younger Brother",
     title: "大爺 / 二爺 ...",
+    engTitle: "Granduncle",
   },
   {
     relationship: "Father's Father's Younger Brother's Wife",
     title: "大奶奶 / 二奶奶...",
+    engTitle: "Grandaunt-in-law",
   },
   {
     relationship: "Father's Mother's Older Sister",
     title: "姨奶奶",
+    engTitle: "Grandaunt",
   },
   {
     relationship: "Father's Mother's Older Sister's Husband",
     title: "姨爺",
+    engTitle: "Granduncle-in-law",
   },
   {
     relationship: "Father's Mother's Older Brother",
     title: "舅爷",
+    engTitle: "Granduncle",
   },
   {
     relationship: "Father's Mother's Older Brother's Wife",
     title: "舅奶奶",
+    engTitle: "Grandaunt-in-law",
   },
   {
     relationship: "Mothers Father's Older Sister",
     title: "姑姥姥",
+    engTitle: "Grandaunt",
   },
   {
     relationship: "Mothers Father's Older Sister Husband",
     title: "姑姥爺",
+    engTitle: "Granduncle-in-law",
   },
   {
     relationship: "Mothers Mother's Older Sister",
     title: "姨姥姥",
+    engTitle: "Grandaunt",
   },
   {
     relationship: "Mother's Mother's Older Sister's Husband",
     title: "姨姥爺",
+    engTitle: "Granduncle-in-law",
   },
   {
     relationship: "Mother's Father's Older Brother",
     title: "舅姥爺",
+    engTitle: "Granduncle",
   },
   {
     relationship: "Mother's Father's Younger Brother's Wife",
     title: "舅姥姥",
+    engTitle: "Grandaunt-in-law",
   },
   {
     relationship: "Father's Mother's Older Sister",
     title: "姨奶奶",
+    engTitle: "Grandaunt",
   },
   {
     relationship: "Father's Mother's Younger Sister",
     title: "姨奶奶",
+    engTitle: "Grandaunt",
   },
   {
     relationship: "Father's Mother's Older Sister's Husband",
     title: "姨爺",
+    engTitle: "Granduncle-in-law",
   },
   {
     relationship: "Father's Mother's Younger Sister's Husband",
     title: "姨爺",
+    engTitle: "Granduncle-in-law",
   },
   {
     relationship: "Father's Mother's Older Brother",
     title: "舅爷",
+    engTitle: "Granduncle",
   },
   {
     relationship: "Father's Mother's Older Brother's Wife",
     title: "舅奶奶",
+    engTitle: "Grandaunt-in-law",
   },
   {
     relationship: "Mothers Father's Younger Sister",
     title: "姑姥姥",
+    engTitle: "Grandaunt",
   },
   {
     relationship: "Mothers Father's Younger Sister Husband",
     title: "姑姥爺",
+    engTitle: "Granduncle-in-law",
   },
   {
     relationship: "Mothers Mother's Younger Sister",
     title: "姨姥姥",
+    engTitle: "Grandaunt",
   },
   {
     relationship: "Mother's Mother's Younger Sister's Husband",
     title: "姨姥爺",
+    engTitle: "Granduncle-in-law",
   },
   {
     relationship: "Mother's Father's Younger Brother",
     title: "舅姥爺",
+    engTitle: "Granduncle",
   },
   {
     relationship: "Mother's Father's Younger Brother's Wife",
     title: "舅姥姥",
+    engTitle: "Grandaunt-in-law",
   },
   // Great Grandparents
   {
     relationship: "Father's Father's Father",
     title: "太爺",
+    engTitle: "Paternal Great-Grandfather",
   },
   {
     relationship: "Father's Mother's Father",
     title: "太爺",
+    engTitle: "Paternal Great-Grandfather",
   },
   {
     relationship: "Father's Father's Mother",
-    title: "太太",
+    title: "太嫲",
+    engTitle: "Paternal Great-Grandmother",
   },
   {
     relationship: "Father's Mother's Mother",
-    title: "太太",
+    title: "太嫲",
+    engTitle: "Paternal Great-Grandmother",
   },
   {
     relationship: "Mother's Father's Father",
     title: "太姥爷",
+    engTitle: "Maternal Great-Grandfather",
   },
   {
     relationship: "Mother's Mother's Father",
     title: "太姥爷",
+    engTitle: "Maternal Great-Grandfather",
   },
   {
     relationship: "Mother's Father's Mother",
     title: "太姥姥",
+    engTitle: "Maternal Great-Grandmother",
   },
   {
     relationship: "Mother's Mother's Mother",
     title: "太姥姥",
+    engTitle: "Maternal Great-Grandmother",
   },
-  //   姥爷 （lǎo ye）: the mum of your dad (your paternal grandmother); 外祖父（wài zǔ fù）is the formal way to say 姥爷（lǎo ye）.
-  // 姥姥 （lǎo lao）: the mum of your mum (your maternal grandmother); 外祖母（wài zǔ mǔ）is the formal way to say 姥姥（lǎo lao）.
-
-  // 太爷 （tài yé）: the dad of your paternal grandfather or grandmother (your paternal great-grandfather)
-
-  // 太太（tài tai）：the mum of your paterrnal grandfather or grandmother (your paternal great-grandmother)
-
-  // 太姥爷（tài lǎo ye）：the dad of you maternal grandfather or grandmother (your paternal great-grandfather)
-  // 太姥姥（tài lǎo lao）：the mum of your maternal grandfather or grandmother (your maternal great-grandparents)
+  // Your children
+  {
+    relationship: "Son's Older Sister",
+    title: "女兒",
+    engTitle: "Daughter",
+  },
+  {
+    relationship: "Son's Younger Sister",
+    title: "女兒",
+    engTitle: "Daughter",
+  },
+  {
+    relationship: "Daughter's Older Sister",
+    title: "女兒",
+    engTitle: "Daughter",
+  },
+  {
+    relationship: "Daughter's Younger Sister",
+    title: "女兒",
+    engTitle: "Daughter",
+  },
+  {
+    relationship: "Son's Older Brother",
+    title: "兒子",
+    engTitle: "Son",
+  },
+  {
+    relationship: "Son's Younger Brother",
+    title: "兒子",
+    engTitle: "Son",
+  },
+  {
+    relationship: "Daughter's Older Brother",
+    title: "兒子",
+    engTitle: "Son",
+  },
+  {
+    relationship: "Daughter's Younger Brother",
+    title: "兒子",
+    engTitle: "Son",
+  },
+  // Your Grandchildren
+  {
+    relationship: "Son's Son",
+    title: "孫子",
+    engTitle: "Grandson",
+  },
+  {
+    relationship: "Son's Daughter",
+    title: "孫女",
+    engTitle: "Granddaughter",
+  },
+  {
+    relationship: "Daughter's Son",
+    title: "孫子",
+    engTitle: "Grandson",
+  },
+  {
+    relationship: "Daughter's Daughter",
+    title: "孫女",
+    engTitle: "Granddaughter",
+  },
+  // Your Great-Grandchildren
+  {
+    relationship: "Son's Son's Son",
+    title: "曾孙子",
+    engTitle: "Great-Grandson",
+  },
+  {
+    relationship: "Son's Son's Daughter",
+    title: "曾孙女",
+    engTitle: "Great-Granddaughter",
+  },
+  {
+    relationship: "Son's Daughter's Son",
+    title: "曾孙子",
+    engTitle: "Great-Grandson",
+  },
+  {
+    relationship: "Son's Daughter's Daughter",
+    title: "曾孙女",
+    engTitle: "Great-Granddaughter",
+  },
+  {
+    relationship: "Daughter's Son's Son",
+    title: "曾孙子",
+    engTitle: "Great-Grandson",
+  },
+  {
+    relationship: "Daughter's Son's Daughter",
+    title: "曾孙女",
+    engTitle: "Great-Granddaughter",
+  },
+  {
+    relationship: "Daughter's Daughter's Son",
+    title: "曾孙子",
+    engTitle: "Great-Grandson",
+  },
+  {
+    relationship: "Daughter's Daughter's Daughter",
+    title: "曾孙女",
+    engTitle: "Great-Granddaughter",
+  },
 ];
